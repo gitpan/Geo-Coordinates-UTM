4094d6e632529eea9b1623fa111088e17d632bda
Graham Crookham <grahamc@cpan.org>
2004-03-23 18:54:50 +0000

    Import of GRAHAMC/Geo-Coordinates-UTM-0.04 from CPAN.
    gitpan-cpan-distribution: Geo-Coordinates-UTM
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         GRAHAMC/Geo-Coordinates-UTM-0.04.tar.gz
    gitpan-cpan-author:       GRAHAMC
    gitpan-cpan-maturity:     released

--- UTM.pm
+++ UTM.pm
@@ -1,206 +1,239 @@
-package Geo::Coordinates::UTM;
-
 use strict;
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+use warnings;
 
-require 5.005;
-require Exporter;
-require AutoLoader;
+package Geo::Coordinates::UTM;
+use base 'Exporter';
+
+our @EXPORT  = qw( latlon_to_utm  utm_to_latlon
+                   ellipsoid_info ellipsoid_names );
+
+our $VERSION = '0.04';
 
 use Math::Trig;
+my $deg2rad =  pi / 180;
+my $rad2deg = 180 /  pi;
 
-@ISA = qw(Exporter AutoLoader);
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-@EXPORT = qw(
-	     latlon_to_utm
-	     utm_to_latlon
-	     );
-
-$VERSION = '0.03';
-
-my $pi = 3.14159265;
-my $quarterpi = $pi / 4;
-my $deg2rad = $pi / 180;
-my $rad2deg = 180 / $pi;
-
-# Ellipsoid array (name,equatorial radius,square of eccentricity)
-
-my @Ellipsoid=([ "Blank", 0, 0],
-	       [ "Airy", 6377563, 0.00667054],
-	       [ "Australian National", 6378160, 0.006694542],
-	       [ "Bessel 1841", 6377397, 0.006674372],
-	       [ "Bessel 1841 (Nambia) ", 6377484, 0.006674372],
-	       [ "Clarke 1866", 6378206, 0.006768658],
-	       [ "Clarke 1880", 6378249, 0.006803511],
-	       [ "Everest", 6377276, 0.006637847],
-	       [ "Fischer 1960 (Mercury) ", 6378166, 0.006693422],
-	       [ "Fischer 1968", 6378150, 0.006693422],
-	       [ "GRS 1967", 6378160, 0.006694605],
-	       [ "GRS 1980", 6378137, 0.00669438],
-	       [ "Helmert 1906", 6378200, 0.006693422],
-	       [ "Hough", 6378270, 0.00672267],
-	       [ "International", 6378388, 0.00672267],
-	       [ "Krassovsky", 6378245, 0.006693422],
-	       [ "Modified Airy", 6377340, 0.00667054],
-	       [ "Modified Everest", 6377304, 0.006637847],
-	       [ "Modified Fischer 1960", 6378155, 0.006693422],
-	       [ "South American 1969", 6378160, 0.006694542],
-	       [ "WGS 60", 6378165, 0.006693422],
-	       [ "WGS 66", 6378145, 0.006694542],
-	       [ "WGS-72", 6378135, 0.006694318],
-	       [ "WGS-84", 6378137, 0.00669438]);
-
-sub latlon_to_utm {
-    # Expects Ellipsoid Number, Latitude, Longitude 
-    # (Latitude and Longitude in decimal degrees)
-    # Returns UTM Zone, UTM Easting, UTM Northing
-    my (@args)=@_;
-    my $ellipsoid=$args[0];
-    my $latitude=$args[1];
-    my $longitude=$args[2];
-    if (($ellipsoid <1) or ($ellipsoid >23)) {
-	die "Ellipsoid value (".$ellipsoid.") invalid.";
+# remove all markup from an ellipsoid name, to increase the chance
+# that a match is found.
+sub _cleanup_name($)
+{   my $copy = lc(shift);
+    for($copy)
+    {   s/\([^)]+\)//g;   # remove text between parantheses
+        s/[\s-]//g;      # no blanks or dashes
     }
-    if  (($longitude < -180) or ($longitude > 180)) {
-	die "Longitude value (".$longitude.") invalid.";
-    }
-    my $radius=$Ellipsoid[$ellipsoid][1];
-    my $eccentricity=$Ellipsoid[$ellipsoid][2];
-    my $k0=0.9996;
-    my $long2=(($longitude + 180) - int (($longitude + 180)/360 ) * 360 - 180);
-    my $zone=(int(($long2 + 180)/6) + 1);
-    my $lat_radian=($latitude * $deg2rad);
-    my $long_radian=($long2 * $deg2rad);
-    if (($latitude >= 56.0) and ($latitude < 64.0) and ($long2 >= 3.0) and ($long2 < 12.0)) {
-	$zone = 32;
-    }
-    if(( $latitude >= 72.0) and ($latitude < 84.0)) { 
-	if(($long2 >= 0.0) and ($long2 <  9.0)) {
-	    $zone = 31;
-	} elsif(($long2 >= 9.0) and ($long2 < 21.0)) {
-	    $zone = 33;
-	} elsif(($long2 >= 21.0) and ($long2 < 33.0)) {
-	    $zone = 35;
-	} elsif(($long2 >= 33.0) and ($long2 < 42.0)) {
-	    $zone = 37;
-	}
-    }    
-    my $longorigin = (($zone - 1)*6 - 180 + 3);
-    my $longoriginradian = $longorigin * $deg2rad;
-    my $eccentprime=($eccentricity/(1-$eccentricity));
-    
-    my $N = ($radius/sqrt(1-$eccentricity * sin($lat_radian) * sin($lat_radian)));
-    my $T = (tan($lat_radian) * tan($lat_radian));
-    my $C = ($eccentprime*cos($lat_radian)*cos($lat_radian));
-    my $A = (cos($lat_radian)*($long_radian-$longoriginradian));
-    my $M = ($radius * ((1 - $eccentricity/4 - 3 * $eccentricity * $eccentricity/64 - 5 * $eccentricity * $eccentricity * $eccentricity/256) * $lat_radian - (3 * $eccentricity/8 + 3 * $eccentricity * $eccentricity/32 + 45 * $eccentricity * $eccentricity * $eccentricity/1024) * sin(2 * $lat_radian) + (15 * $eccentricity * $eccentricity/256 + 45 * $eccentricity * $eccentricity * $eccentricity/1024) * sin(4 * $lat_radian) - (35 * $eccentricity * $eccentricity * $eccentricity/3072) * sin(6 * $lat_radian)));
-    my $utm_easting=($k0*$N*($A+(1-$T+$C)*$A*$A*$A/6 + (5-18*$T+$T*$T+72*$C-58*$eccentprime)*$A*$A*$A*$A*$A/120) + 500000.0);
-    my $utm_northing=($k0*($M+$N*tan($lat_radian)*($A*$A/2+(5-$T+9*$C+4*$C*$C)*$A*$A*$A*$A/24 + (61-58*$T+$T*$T+600*$C-330*$eccentprime)*$A*$A*$A*$A*$A*$A/720)));
-    if($latitude < 0) {
-	$utm_northing += 10000000.0; 
-    }
-    my $utm_letter;
-    if ((84 >= $latitude) and ($latitude >= 72)) {
-	$utm_letter = 'X';
-    } elsif ((72 > $latitude) && ($latitude >= 64)) {
-	$utm_letter = 'W';
-    } elsif ((64 > $latitude) && ($latitude >= 56)) {
-	$utm_letter = 'V';
-    } elsif ((56 > $latitude) && ($latitude >= 48)) {
-	$utm_letter = 'U';
-    } elsif ((48 > $latitude) && ($latitude >= 40)) {
-	$utm_letter = 'T';
-    } elsif ((40 > $latitude) && ($latitude >= 32)) {
-	$utm_letter = 'S';
-    } elsif ((32 > $latitude) && ($latitude >= 24)) {
-	$utm_letter = 'R';
-    } elsif ((24 > $latitude) && ($latitude >= 16)) {
-	$utm_letter = 'Q';
-    } elsif ((16 > $latitude) && ($latitude >= 8)) {
-	$utm_letter = 'P';
-    } elsif (( 8 > $latitude) && ($latitude >= 0)) {
-	$utm_letter = 'N';
-    } elsif (( 0 > $latitude) && ($latitude >= -8)) {
-	$utm_letter = 'M';
-    } elsif ((-8> $latitude) && ($latitude >= -16)) {
-	$utm_letter = 'L';
-    } elsif ((-16 > $latitude) && ($latitude >= -24)) {
-	$utm_letter = 'K';
-    } elsif ((-24 > $latitude) && ($latitude >= -32)) {
-	$utm_letter = 'J';
-    } elsif ((-32 > $latitude) && ($latitude >= -40)) {
-	$utm_letter = 'H';
-    } elsif ((-40 > $latitude) && ($latitude >= -48)) {
-	$utm_letter = 'G';
-    } elsif ((-48 > $latitude) && ($latitude >= -56)) {
-	$utm_letter = 'F';
-    } elsif ((-56 > $latitude) && ($latitude >= -64)) {
-	$utm_letter = 'E';
-    } elsif ((-64 > $latitude) && ($latitude >= -72)) {
-	$utm_letter = 'D';
-    } elsif ((-72 > $latitude) && ($latitude >= -80)) {
-	$utm_letter = 'C';
-    } else {
-	die "Latitude (".$latitude.") out of UTM range.";
-    }
-    $zone.=$utm_letter;
-    return $zone,$utm_easting,$utm_northing;
+    $copy;
 }
 
-sub utm_to_latlon {
-    # Expects Ellipsoid Number, UTM zone, UTM Easting, UTM Northing
-    # Returns Latitude, Longitude
-    # (Latitude and Longitude in decimal degrees, UTM Zone e.g. 23S)
-    my (@args)=@_;
-    my $valid_utm="CDEFGHJKLMNPQRSTUVWX";
-    my $ellipsoid=$args[0];
-    my $zone=$args[1];
-    my $easting=$args[2];
-    my $northing=$args[3];
-    my $zone_letter=substr($zone,(length($zone)-1),1);
-    my $ZoneNumber=substr($zone,0,(length($zone)-1));
-    if (($ellipsoid <1) or ($ellipsoid >23)) {
-	die "Ellipsoid value (".$ellipsoid.") invalid.";
+# Ellipsoid array (name,equatorial radius,square of eccentricity)
+# Same data also as hash with key eq name (in variations)
+
+my (@Ellipsoid, %Ellipsoid);
+
+
+BEGIN {  # Initialize this before other modules get a chance
+   @Ellipsoid =
+    ( [ "Airy", 6377563, 0.00667054]
+    , [ "Australian National", 6378160, 0.006694542]
+    , [ "Bessel 1841", 6377397, 0.006674372]
+    , [ "Bessel 1841 (Nambia) ", 6377484, 0.006674372]
+    , [ "Clarke 1866", 6378206, 0.006768658]
+    , [ "Clarke 1880", 6378249, 0.006803511]
+    , [ "Everest", 6377276, 0.006637847]
+    , [ "Fischer 1960 (Mercury) ", 6378166, 0.006693422]
+    , [ "Fischer 1968", 6378150, 0.006693422]
+    , [ "GRS 1967", 6378160, 0.006694605]
+    , [ "GRS 1980", 6378137, 0.00669438]
+    , [ "Helmert 1906", 6378200, 0.006693422]
+    , [ "Hough", 6378270, 0.00672267]
+    , [ "International", 6378388, 0.00672267]
+    , [ "Krassovsky", 6378245, 0.006693422]
+    , [ "Modified Airy", 6377340, 0.00667054]
+    , [ "Modified Everest", 6377304, 0.006637847]
+    , [ "Modified Fischer 1960", 6378155, 0.006693422]
+    , [ "South American 1969", 6378160, 0.006694542]
+    , [ "WGS 60", 6378165, 0.006693422]
+    , [ "WGS 66", 6378145, 0.006694542]
+    , [ "WGS-72", 6378135, 0.006694318]
+    , [ "WGS-84", 6378137, 0.00669438 ]
+    );
+
+  foreach my $el (@Ellipsoid)
+  {   my ($name, $eqrad, $eccsq) = @$el;
+      $Ellipsoid{$name} = $el;
+      $Ellipsoid{_cleanup_name $name} = $el;
+  }
+}
+
+sub _valid_utm_zone($)
+{   my $char = shift;
+    index("CDEFGHJKLMNPQRSTUVWX", $char) >= 0;
+}
+
+# Returns all pre-defined ellipsoid names, sorted alphabetically
+sub ellipsoid_names()
+{   map { $_->[0] } @Ellipsoid;
+}
+
+# Returns "official" name, equator radius and square eccentricity
+# The specified name can be numeric (for compatibility reasons) or
+# a more-or-less exact name
+# Examples:   my($name, $r, $sqecc) = ellipsoid_info 'wgs84';
+#             my($name, $r, $sqecc) = ellipsoid_info 'WGS 84';
+#             my($name, $r, $sqecc) = ellipsoid_info 'WGS-84';
+#             my($name, $r, $sqecc) = ellipsoid_info 'WGS-84 (new specs)';
+#             my($name, $r, $sqecc) = ellipsoid_info 22;
+
+sub ellipsoid_info($)
+{   my $id = shift;
+
+    my $el = $id !~ m/\D/
+           ? $Ellipsoid[$id-1]   # old system counted from 1
+           : $Ellipsoid{$id} || $Ellipsoid{_cleanup_name $id};
+
+    defined $el ? @$el : ();
+}
+
+# Expects Ellipsoid Number or name, Latitude, Longitude 
+# (Latitude and Longitude in decimal degrees)
+# Returns UTM Zone, UTM Easting, UTM Northing
+
+sub latlon_to_utm($$$)
+{   my ($ellips, $latitude, $longitude) = @_;
+
+    my ($name, $radius, $eccentricity) = ellipsoid_info $ellips
+        or die "Ellipsoid value ($ellips) invalid.";
+
+    die "Longitude value ($longitude) invalid."
+        if $longitude < -180 || $longitude > 180;
+
+#>> original line contained +180 -180
+    my $long2       = $longitude - int(($longitude + 180)/360) * 360;
+    my $zone        = int( ($long2 + 180)/6) + 1;
+
+    my $lat_radian  = $deg2rad * $latitude;
+    my $long_radian = $deg2rad * $long2;
+
+    my $k0          = 0.9996;   # scale
+
+    if($latitude >= 56.0 && $latitude < 64.0 && $long2 >= 3.0 && $long2 < 12.0)
+    {   $zone = 32;
     }
-    if ($valid_utm!~ /$zone_letter/) {
-	die "UTM zone (".$zone_letter.") invalid.";
+    if($latitude >= 72.0 && $latitude < 84.0) { 
+        $zone = ($long2 >=  0.0 && $long2 <  9.0) ? 31
+              : ($long2 >=  9.0 && $long2 < 21.0) ? 33
+              : ($long2 >= 21.0 && $long2 < 33.0) ? 35
+              : ($long2 >= 33.0 && $long2 < 42.0) ? 37
+              :                                     undef;
     }
-    my $radius=$Ellipsoid[$ellipsoid][1];
-    my $eccentricity=$Ellipsoid[$ellipsoid][2];
-    my $k0=0.9996;
-    my $e1 = (1-sqrt(1-$eccentricity))/(1+sqrt(1-$eccentricity));
-    my $x = $easting - 500000; # Remove Longitude offset
-    my $y = $northing;
+
+    my $longorigin       = ($zone - 1)*6 - 180 + 3;
+    my $longoriginradian = $deg2rad * $longorigin;
+    my $eccentprime      = $eccentricity/(1-$eccentricity);
+    
+    my $N = $radius / sqrt(1-$eccentricity * sin($lat_radian)*sin($lat_radian));
+    my $T = tan($lat_radian) * tan($lat_radian);
+    my $C = $eccentprime * cos($lat_radian)*cos($lat_radian);
+    my $A = cos($lat_radian) * ($long_radian - $longoriginradian);
+    my $M = $radius
+            * ( ( 1 - $eccentricity/4 - 3 * $eccentricity * $eccentricity/64
+                  - 5 * $eccentricity * $eccentricity * $eccentricity/256
+                ) * $lat_radian
+              - ( 3 * $eccentricity/8 + 3 * $eccentricity * $eccentricity/32
+                  + 45 * $eccentricity * $eccentricity * $eccentricity/1024
+                ) * sin(2 * $lat_radian)
+              + ( 15 * $eccentricity * $eccentricity/256 +
+                  45 * $eccentricity * $eccentricity * $eccentricity/1024
+                ) * sin(4 * $lat_radian)
+              - ( 35 * $eccentricity * $eccentricity * $eccentricity/3072
+                ) * sin(6 * $lat_radian)
+              );
+
+    my $utm_easting = $k0*$N*($A+(1-$T+$C)*$A*$A*$A/6
+                    + (5-18*$T+$T*$T+72*$C-58*$eccentprime)*$A*$A*$A*$A*$A/120)
+                    + 500000.0;
+
+    my $utm_northing= $k0 * ( $M + $N*tan($lat_radian) * ( $A*$A/2+(5-$T+9*$C+4*$C*$C)*$A*$A*$A*$A/24 + (61-58*$T+$T*$T+600*$C-330*$eccentprime) * $A*$A*$A*$A*$A*$A/720));
+
+    $utm_northing += 10000000.0 if $latitude < 0;
+
+    my $utm_letter
+      = ( 84 >= $latitude && $latitude >=  72) ? 'X'
+      : ( 72 >  $latitude && $latitude >=  64) ? 'W'
+      : ( 64 >  $latitude && $latitude >=  56) ? 'V'
+      : ( 56 >  $latitude && $latitude >=  48) ? 'U'
+      : ( 48 >  $latitude && $latitude >=  40) ? 'T'
+      : ( 40 >  $latitude && $latitude >=  32) ? 'S'
+      : ( 32 >  $latitude && $latitude >=  24) ? 'R'
+      : ( 24 >  $latitude && $latitude >=  16) ? 'Q'
+      : ( 16 >  $latitude && $latitude >=   8) ? 'P'
+      : (  8 >  $latitude && $latitude >=   0) ? 'N'
+      : (  0 >  $latitude && $latitude >=  -8) ? 'M'
+      : ( -8 >  $latitude && $latitude >= -16) ? 'L'
+      : (-16 >  $latitude && $latitude >= -24) ? 'K'
+      : (-24 >  $latitude && $latitude >= -32) ? 'J'
+      : (-32 >  $latitude && $latitude >= -40) ? 'H'
+      : (-40 >  $latitude && $latitude >= -48) ? 'G'
+      : (-48 >  $latitude && $latitude >= -56) ? 'F'
+      : (-56 >  $latitude && $latitude >= -64) ? 'E'
+      : (-64 >  $latitude && $latitude >= -72) ? 'D'
+      : (-72 >  $latitude && $latitude >= -80) ? 'C'
+      : die "Latitude ($latitude) out of UTM range.";
+
+    $zone .= $utm_letter;
+
+    ($zone, $utm_easting, $utm_northing);
+}
+
+# Expects Ellipsoid Number or name, UTM zone, UTM Easting, UTM Northing
+# Returns Latitude, Longitude
+# (Latitude and Longitude in decimal degrees, UTM Zone e.g. 23S)
+
+sub utm_to_latlon($$$$)
+{   my ($ellips, $zone, $easting, $northing) = @_;
+
+    my ($name, $radius, $eccentricity) = ellipsoid_info $ellips
+        or die "Ellipsoid value ($ellips) invalid.";
+       
+    my $zone_number = $zone;
+    my $zone_letter = chop $zone_number;
+
+    die "UTM zone ($zone_letter) invalid."
+       unless _valid_utm_zone $zone_letter;
+
+    my $k0 = 0.9996;
+    my $x  = $easting - 500000; # Remove Longitude offset
+    my $y  = $northing;
+
     # Set hemisphere (1=Northern, 0=Southern)
-    my $hemisphere;
-    if (("NPQRSTUVWX" =~ $zone_letter) > 0) {
-	$hemisphere = 1;
-    } else {
-	$hemisphere = 0;
-	$y -= 10000000.0; # Remove Southern Offset
-    }
-    my $longorigin=($ZoneNumber - 1)*6 - 180 + 3;
+    my $hemisphere = $zone_letter ge 'N';
+    $y    -= 10000000.0 unless $hemisphere; # Remove Southern Offset
+
+    my $longorigin      = ($zone_number - 1)*6 - 180 + 3;
     my $eccPrimeSquared = ($eccentricity)/(1-$eccentricity);
-    my $M = ($y/$k0);
+    my $M  = $y/$k0;
     my $mu = $M/($radius*(1-$eccentricity/4-3*$eccentricity*$eccentricity/64-5*$eccentricity*$eccentricity*$eccentricity/256));
+
+    my $e1 = (1-sqrt(1-$eccentricity))/(1+sqrt(1-$eccentricity));
     my $phi1rad = $mu+(3*$e1/2-27*$e1*$e1*$e1/32)*sin(2*$mu)+(21*$e1*$e1/16-55*$e1*$e1*$e1*$e1/32)*sin(4*$mu)+(151*$e1*$e1*$e1/96)*sin(6*$mu);
     my $phi1 = $phi1rad*$rad2deg;
     my $N1 = $radius/sqrt(1-$eccentricity*sin($phi1rad)*sin($phi1rad));
     my $T1 = tan($phi1rad)*tan($phi1rad);
     my $C1 = $eccentricity*cos($phi1rad)*cos($phi1rad);
-    my $R1 = $radius*(1-$eccentricity)/((1-$eccentricity*sin($phi1rad)*sin($phi1rad))**1.5);
+    my $R1 = $radius * (1-$eccentricity)
+             / ((1-$eccentricity*sin($phi1rad)*sin($phi1rad))**1.5);
     my $D = $x/($N1*$k0);
 
     my $Latitude = $phi1rad-($N1*tan($phi1rad)/$R1)*($D*$D/2-(5+3*$T1+10*$C1-4*$C1*$C1-9*$eccPrimeSquared)*$D*$D*$D*$D/24+(61+90*$T1+298*$C1+45*$T1*$T1-252*$eccPrimeSquared-3*$C1*$C1)*$D*$D*$D*$D*$D*$D/720);
     $Latitude = $Latitude * $rad2deg;
+
     my $Longitude = ($D-(1+2*$T1+$C1)*$D*$D*$D/6+(5-2*$C1+28*$T1-3*$C1*$C1+8*$eccPrimeSquared+24*$T1*$T1)*$D*$D*$D*$D*$D/120)/cos($phi1rad);
     $Longitude = $longorigin + $Longitude * $rad2deg;
-    return $Latitude, $Longitude;
+
+    ($Latitude, $Longitude);
 }
 
 1;
+
 __END__
 
 =head1 NAME
@@ -211,9 +244,13 @@ Geo::Coordinates::UTM - Perl extension for Latitiude Longitude conversions.
 
 use Geo::Coordinates::UTM;
 
-($zone,$easting,$northing)=latlon_to_utm($ellipsoid,$latitude,$longitude);
+my ($zone,$easting,$northing)=latlon_to_utm($ellipsoid,$latitude,$longitude);
 
-($latitude,$longitude)=utm_to_latlon($ellipsoid,$zone,$easting,$northing);
+my ($latitude,$longitude)=utm_to_latlon($ellipsoid,$zone,$easting,$northing);
+
+my @ellipsoids=ellipsoid_names;
+
+my($name, $r, $sqecc) = ellipsoid_info 'WGS-84';
 
 =head1 DESCRIPTION
 
@@ -254,7 +291,7 @@ A description of the available ellipsoids and sample usage of the conversion rou
 
 =head2 Ellipsoids
 
-The Ellipsoids available are numbered as follows:
+The Ellipsoids available are as follows:
 
 =over 6
 
@@ -305,7 +342,23 @@ The Ellipsoids available are numbered as follows:
 =item 23 WGS-84
 
 =back
- 
+
+
+=head2 ellipsoid_names
+
+The ellipsoids can be accessed using  ellipsoid_names. To store thes into an array you could use 
+
+     my @names = ellipsoid_names;
+
+=head2 ellipsoid_info
+
+Ellipsoids may be called either by name, or number. To return the ellipsoid information, ( "official" name, equator radius and square eccentricity) you can use ellipsoid_info and specify a name. The specified name can be numeric (for compatibility reasons) or a more-or-less exact name. Any text between parentheses will be ignored.
+
+     my($name, $r, $sqecc) = ellipsoid_info 'wgs84';
+     my($name, $r, $sqecc) = ellipsoid_info 'WGS 84';
+     my($name, $r, $sqecc) = ellipsoid_info 'WGS-84';
+     my($name, $r, $sqecc) = ellipsoid_info 'WGS-84 (new specs)';
+     my($name, $r, $sqecc) = ellipsoid_info 23;
 
 =head2 latlon_to_utm
 
@@ -318,7 +371,7 @@ For latitude  57deg 49min 59.000sec North
 
 using Clarke 1866 (Ellipsoid 5)
 
-     ($zone,$east,$north)=latlon_to_utm(5,57.803055556,-2.788951667)
+     ($zone,$east,$north)=latlon_to_utm('clarke 1866',57.803055556,-2.788951667)
 
 returns 
 
@@ -346,9 +399,19 @@ returns
 
 Graham Crookham, grahamc@cpan.org
 
+=head1 THANKS
+
+Thanks go to the following:
+
+Felipe Mendonca Pimenta for helping out with the Southern hemisphere testing.
+
+Michael Slater for discovering the Escape \Q bug.
+
+Mark Overmeer for the ellipsoid_info routines and code review.
+
 =head1 COPYRIGHT
 
-Copyright (c) 2000,2002 by Graham Crookham.  All rights reserved.
+Copyright (c) 2000,2002,2004 by Graham Crookham.  All rights reserved.
     
 This package is free software; you can redistribute it
 and/or modify it under the same terms as Perl itself.             
